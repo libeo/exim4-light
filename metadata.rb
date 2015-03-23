da27848e216529125aa651438531ced930412357
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-03-23 15:09:59 -0400

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "thomas.massmann@inqbus.de"
 license           "Apache v2.0"
 description       "Installs and configures exim4-light."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.3"
+version           "0.1.4"
 
 %w{ debian ubuntu }.each do |os|
   supports os
